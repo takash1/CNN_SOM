e361aa53ae2e1b1366d22e44da3ee9ef3eb9d6d4
Takashi Ueno <ueno.takashi.75w@st.kyoto-u.ac.jp>
2017-02-23 01:39:31 +0900

    add comment

--- SOM/music_som.R
+++ SOM/music_som.R
@@ -3,7 +3,10 @@ data <- read.csv("features.csv", header=F)
 set.seed(10)
 music_som <- som(scale(data[,2:101]), grid=somgrid(5, 5, "hexagonal"), rlen=3000)
 
-#plot(music_som, type="changes", main="chage of similarity")
+# Training progress
+# plot(music_som, type="changes", main="chage of similarity")
+
+# Visualize
 png("som.png")
 plot(music_som, type="mapping", labels=data[,1], main="X Japan SOM")
 dev.off()
